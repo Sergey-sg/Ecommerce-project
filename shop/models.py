03b4fa5e7777d41ffeae393f46b30efa99024a6b
Sergey Glushak <glushak_sergey@i.ua>
2021-11-24 00:42:36 +0200

    fix product and home page, add categories to navbar, creat context_processors.py, add to Category and Product class method get_url

--- shop/models.py
+++ shop/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 from django.urls import reverse
-import uuid
-from datetime import date
-from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -15,6 +12,9 @@ class Category(models.Model):
         ordering = ('name', )
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_by_categories', args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -36,6 +36,9 @@ class Product(models.Model):
         ordering = ('name', )
         verbose_name = 'product'
         verbose_name_plural = 'products'
+
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.name
