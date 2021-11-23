83094eaac9b1bf68bc15fbd2482330dc27d40e94
Sergey Glushak <glushak_sergey@i.ua>
2021-11-23 23:27:12 +0200

    creat Product and Category models, add products to home page

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('product/', views.product, name='product'),
+    path('<slug:category_slug>', views.home, name='products_by_categories'),
 ]
